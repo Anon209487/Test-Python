07a1299e1de613cad107aaa287bbe136843a3b09
Neos Rafael Ruiz Sanchez <156091669+Anon209487@users.noreply.github.com>
2024-10-10 10:12:56 +0200

    aa

--- test_encriptacion_mensaje.py
+++ test_encriptacion_mensaje.py
@@ -5,3 +5,12 @@ def test_encriptacion_mensaje():
 
 def test_encriptacion_mensaje2():
     assert encriptacion_mensaje('abcd',1)=='bcde'
+
+def test_encriptacion_mensaje3():
+    assert encriptacion_mensaje('abcd',2)=='cdef'
+
+def test_encriptacion_mensaje4():
+    assert encriptacion_mensaje('z',1)=='a'
+    
+def test_encriptacion_mensaje5():
+    assert encriptacion_mensaje('b',-1)=='a'
