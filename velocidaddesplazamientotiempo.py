5084fd5a042a9bb73255e0532d924edca91f4948
Neos Rafael Ruiz Sanchez <156091669+Anon209487@users.noreply.github.com>
2024-10-10 11:50:06 +0200

    AA

---
+++ velocidaddesplazamientotiempo.py
@@ -0,0 +1,9 @@
+
+def velocidaddesplazamientotiempo(dato1, dato2):
+    res = ""
+    mensaje=mensaje.lower()
+    abecedario = 'abcdefghijklmnopqrstuvwxyz'  
+    letra = dato1.index(0)
+    letra2 = dato2.index(0)
+        
+    return res
